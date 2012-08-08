201293c80123b1f07549229e44e806e32aec2f37
Jahangir Zinedine <jzinedine@gmail.com>
2012-08-08 18:35:14 +0430

    TDS and Axlsx is used

--- profile_read.rb
+++ profile_read.rb
@@ -4,9 +4,10 @@ require 'debugger'
 require 'axlsx'
 require 'roo'
 require 'tiny_tds'
+require 'date'
 
-class Profile_Reader
-
+class Profile_Reader	
+	
 	def initialize(username, password)
 		@username, @password , @nids = username, password, Array.new	
 	end
@@ -36,28 +37,19 @@ class Profile_Reader
 	end
 	
 	def process_file
-		Dir["/Sites/inbox/*.xlsx"].each do |file| 
+		Dir["/Users/Administrator/Documents/GitHub/Profile_Filler/inbox/*.xlsx"].each do |file| 
 			puts "reading file:", file
 			modify_file file
 		end
 	end
 	
-	def modify_file filename
-		debugger
-		p = Excelx.new filename
-		#puts "\t", p.sheets
-		p.sheets.each do |sheet|
-			@nids << p.column(1)
-			break
-		end
-		
+	def get_db_info sh_data
 		client = TinyTds::Client.new(:username => '', 
-									 :password => '@!', 
-									 :host => '2')
-		puts @nids.join(",")
+									 :password => '', 
+									 :host => '')
 		query = "WITH T1 AS( 
 							SELECT 
-								(ROW_NUMBER() OVER(PARTITION BY NationalNo ORDER BY nationalno ) )AS row,
+								(ROW_NUMBER() OVER(PARTITION BY NationalNo ORDER BY NationalNo ) )AS row,
 								registrationlevel, registrationdate, NationalNo,msisdn,firstname,LastName,Nationality, 
 								CASE Gender WHEN 'Male' THEN 'مرد' WHEN 'Female' THEN 'زن' ELSE '' END AS Gender, 
 								CASE Gender WHEN 'Male' THEN 'آقای' WHEN 'Female' THEN 'خانم' ELSE '' END AS Title, 
@@ -83,17 +75,43 @@ class Profile_Reader
 								MobileNo,Tel,Province,City,ad1,Ave,ad2,Street,ad3,Description,Block,BuildingNo,Floor,Unit,MunicipalityRegion,ServiceType,
 								DepositAmount,Service_List,provisiondate,IsCommitted,DepositBank,DepositDate,Packages,Delivery_Method 
 						FROM T1 
-						WHERE nationalno IN ( "+@nids.join(",")+" ) AND registrationlevel =1
+						WHERE nationalno IN ( "+sh_data.transpose.first.join(",")+" ) AND registrationlevel =1
 						ORDER BY NationalNo "
 		puts query
 		result = client.execute(query)
-		debugger
-		result.each { |row| puts row.values } 
-		#p.serialize('simple.xlsx')
+		client.close
+		result
 	end
 	
+	def modify_file filename
+		excel = Excelx.new filename
+		new_excel = Axlsx::Package.new
+		shs_data = {}
+		debugger
+		1.upto(excel.sheets.length) do |i|
+			key = excel.sheets[i].to_s
+			nids = excel.column(1,excel.sheets[i])[2..-1]
+			msisdns = excel.column(2,excel.sheets[i])[2..-1]
+			iccids = excel.column(3,excel.sheets[i])[2..-1]
+			shs_data[key] = nids.zip(msisdns,iccids)
+		end
+		shs_data.each do |sheet_name, sh_data| 
+			result = get_db_info sh_data
+			new_excel.workbook do |wb|
+				wb.add_worksheet(:name => sheet_name) do |ws|
+					result.each_with_index do |row,i| 
+						ws.add_row ["NationalNo", "MSISDN", "ICCID"].concat(row.keys[2..-1]) if i == 0 
+						ws.add_row sh_data[i][1..3].concat(row.values[2..-1])
+					end
+				end
+			end
+		end
+		new_filename = filename.gsub(/inbox\//,'').gsub(/.xlsx/,"_Filled_#{ DateTime.now.to_s.gsub(/:/,'')}.xlsx")
+		puts "writig #{new_filename}"
+		new_excel.serialize "#{new_filename}"
+	end
 end
 
-reader = Profile_Reader.new('j.','@!')
+reader = Profile_Reader.new('j.zinedine','')
 #reader.email_read
 reader.process_file
