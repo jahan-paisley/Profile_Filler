371d08fca6eea8a89a4dc58d9110d552835a6c22
Jahangir Zinedine <jzinedine@gmail.com>
2012-08-10 02:53:51 +0430

    Pop3 is in place, plan tot use imap interface

--- profile_read.rb
+++ profile_read.rb
@@ -13,31 +13,29 @@ class Profile_Reader
 	end
 
 	def email_read
-		pop = Net::POP3.new 'mail.rightel.ir'
+		pop = Net::POP3.new 'mail.tamintelecom.ir'
 		pop.enable_ssl(:verify_mode=> OpenSSL::SSL::VERIFY_NONE)
 		pop.start @username, @password
 		pop.each_mail.reverse_each.each_with_index do |m, i|
 			mail = Mail.new m.pop
 			puts "processing #{mail.subject}"
-			if mail.has_attachments?
-				mail.attachments.each do | attachment |
-				 if (attachment.filename.end_with?('.xlsx'))
-					filename = attachment.filename
-					puts "\twriting attachment: #{filename}"
-					begin
-					 File.open('inbox/' + filename, "w+b", 0644) {|f| f.write attachment.body.decoded}
+			next unless mail.from.to_s.downcase.include? 'bassam' and mail.has_attachments?
+			mail.attachments.select {|att| att.filename.end_with?('.xlsx') and (att.filename.downcase.include?("order") or att.filename.downcase.include?("profile"))}.each do |attachment|
+				filename = attachment.filename
+				puts "\n\twriting attachment: #{filename}"
+				begin
+					File.open('./inbox/' + filename, "w+b", 0644) {|f| f.write attachment.body.decoded}
 					rescue Exception => e
-					 puts "Unable to save data for #{filename} because #{e.message}"
-					end
-				 end
+					  puts "Unable to save data for #{filename} because #{e.message}"
 				end
-			end
-			break if i > 3
-		end	
+		  end
+		end
+			
 	end
 	
 	def process_file
-		Dir["/Users/Administrator/Documents/GitHub/Profile_Filler/inbox/*.xlsx"].each do |file| 
+	  #debugger
+		Dir["./inbox/*.xlsx"].each do |file| 
 			puts "reading file:", file
 			modify_file file
 		end
@@ -47,6 +45,16 @@ class Profile_Reader
 		client = TinyTds::Client.new(:username => '', 
 									 :password => '', 
 									 :host => '')
+		#puts sh_data.first[1].to_s
+		case  
+		  when sh_data.first[1].to_s.start_with?("920")
+		    stype= "'Postpaid'"
+		  when sh_data.first[1].to_s.start_with?("9218")
+		    stype= "'Data'"
+		  else
+		    stype= "'Prepaid'"
+		end 
+
 		query = "WITH T1 AS( 
 							SELECT 
 								(ROW_NUMBER() OVER(PARTITION BY NationalNo ORDER BY NationalNo ) )AS row,
@@ -75,43 +83,49 @@ class Profile_Reader
 								MobileNo,Tel,Province,City,ad1,Ave,ad2,Street,ad3,Description,Block,BuildingNo,Floor,Unit,MunicipalityRegion,ServiceType,
 								DepositAmount,Service_List,provisiondate,IsCommitted,DepositBank,DepositDate,Packages,Delivery_Method 
 						FROM T1 
-						WHERE nationalno IN ( "+sh_data.transpose.first.join(",")+" ) AND registrationlevel =1
-						ORDER BY NationalNo "
-		puts query
-		result = client.execute(query)
-		client.close
-		result
+						WHERE nationalno IN ( "+sh_data.transpose.first.join(",")+" ) AND registrationlevel =1 AND ServiceType=" + stype +" ORDER BY NationalNo"
+		#puts query
+    client.execute(query).to_a
 	end
 	
 	def modify_file filename
 		excel = Excelx.new filename
 		new_excel = Axlsx::Package.new
 		shs_data = {}
-		debugger
+		
+		return if excel.cell(2,'A').downcase.include? 'nationalno' and 
+		#debugger
 		1.upto(excel.sheets.length) do |i|
 			key = excel.sheets[i].to_s
+			next if excel.column(1,excel.sheets[i]) == nil or  !(/\d+/ =~ excel.column(1,excel.sheets[i])[2].to_s) 
 			nids = excel.column(1,excel.sheets[i])[2..-1]
 			msisdns = excel.column(2,excel.sheets[i])[2..-1]
 			iccids = excel.column(3,excel.sheets[i])[2..-1]
 			shs_data[key] = nids.zip(msisdns,iccids)
 		end
 		shs_data.each do |sheet_name, sh_data| 
-			result = get_db_info sh_data
-			new_excel.workbook do |wb|
-				wb.add_worksheet(:name => sheet_name) do |ws|
-					result.each_with_index do |row,i| 
-						ws.add_row ["NationalNo", "MSISDN", "ICCID"].concat(row.keys[2..-1]) if i == 0 
-						ws.add_row sh_data[i][1..3].concat(row.values[2..-1])
-					end
-				end
-			end
-		end
+		  next if sh_data.length < 1 
+		  result = get_db_info sh_data 
+	    new_excel.workbook do |wb|
+			  wb.add_worksheet(:name => sheet_name) do |ws|
+				  result.each_with_index do |row,i| 			  
+					  #debugger
+					  if i == 0 then
+					    ws.add_row excel.row(1)
+					    ws.add_row excel.row(2)
+					  end 
+					  ws.add_row sh_data.find{|item| item[0] == row.values[0]}.concat(row.values[2..-1])
+				  end
+			  end
+		  end
+	  end
+		
 		new_filename = filename.gsub(/inbox\//,'').gsub(/.xlsx/,"_Filled_#{ DateTime.now.to_s.gsub(/:/,'')}.xlsx")
 		puts "writig #{new_filename}"
 		new_excel.serialize "#{new_filename}"
 	end
 end
 
-reader = Profile_Reader.new('j.zinedine','')
-#reader.email_read
+reader = Profile_Reader.new('','')
+reader.email_read
 reader.process_file
