03da8eece7efacfcd92a2ce9463d2e809d600ca5
Jahangir Zinedine <jzinedine@gmail.com>
2012-08-11 00:57:35 +0430

    Refactorings

--- profile_read.rb
+++ profile_read.rb
@@ -9,69 +9,72 @@ require 'tiny_tds'
 require 'date'
 require 'log4r'
 
-
 class Profile_Reader	
-  
+	
 	include Log4r
 	
-	def initialize(username, password)
-		@username, @password , @nids = username, password, Array.new	
-		@log = Logger.new 'log'; @log.outputters = Outputter.stdout
+	def initialize()
+		@email_params = {:username=> '', :password=> '', :host=> ''}
+		@sql_params = {:username=> '', :password=> '', :host=> ''}
+  	@log = Logger.new 'log'; @log.outputters = Outputter.stdout
+  	@path = './inbox/'
+  	@keywords= ['order', 'profile']
 	end
 
-	def email_read
-	  begin
-	    imap = Net::IMAP.new('mail.tamintelecom.ir',:ssl=>{:verify_mode=> OpenSSL::SSL::VERIFY_NONE}) 
-      msg = imap.login(@username, @password)       
-	  rescue Exception => e
-	    puts e
-	  end
-    imap.select("INBOX")
-    latest_saved_file_date = Dir.entries("./inbox/").map { |c| File.stat("./inbox/"+c).ctime }.max
-    start = (latest_saved_file_date || Time.now.prev_year)
-    sstart = start.to_datetime.prev_year.strftime("%d-%b-%Y")
-    eend = Time.now.strftime("%d-%b-%Y")
-    
-    @log.info "start: "+ sstart+ "\tend: "+  eend
-    messages = imap.search(["FROM", "bassam", "BEFORE",eend , "SINCE", sstart])
-    messages.each do |msg_id|
-      mail = Mail.new imap.fetch(msg_id, "RFC822")[0].attr["RFC822"]
-      puts "processing #{mail.subject}"
-			next unless mail.from.to_s.downcase.include? 'bassam' and mail.has_attachments?
-			mail.attachments.select {|att| att.filename.end_with?('.xlsx') and (att.filename.downcase.include?("order") or att.filename.downcase.include?("profile"))}.each do |attachment|
-				filename = attachment.filename
-				puts "\n\twriting attachment: #{filename}"
-				begin
-					File.open('./inbox/' + filename, "w+b", 0644) {|f| f.write attachment.body.decoded}
-					rescue Exception => e
-					  puts "Unable to save data for #{filename} because #{e.message}"
-				end
-		  end
-		end
-		imap.logout 
-    imap.disconnect
+	def set_dates
+		format = "%d-%b-%Y"
+		last_date = Dir.entries(@path).map { |c| File.stat(@path+c).ctime }.max
+		start = (last_date || Time.now.prev_year)
+		sstart = start.to_datetime.prev_year.strftime(format)
+		eend = Time.now.strftime(format)
+		return sstart, eend 
 	end
 	
-	def process_file
-	  
-		Dir["./inbox/*.xlsx"].each do |file| 
+	def email_read
+		begin
+			imap = Net::IMAP.new(@email_params[:host],:ssl=>{:verify_mode=> OpenSSL::SSL::VERIFY_NONE}) 
+			msg = imap.login(@email_params[:username],@email_params[:password])			 
+		rescue Exception=> e
+			puts e
+		end
+		imap.select("INBOX")
+		sstart, eend = set_dates
+		@log.info "start: "+ sstart+ "\tend: "+	 eend
+		messages = imap.search(["FROM", "bassam", "BEFORE",eend , "SINCE", sstart])
+		messages.each do |msg_id|
+			mail = Mail.new imap.fetch(msg_id, "RFC822")[0].attr["RFC822"]
+			puts "processing #{mail.subject}"
+			next unless mail.has_attachments?
+			mail.attachments.select{|att| att.filename.end_with?('.xlsx') and @keywords.any?{|key| att.filename.downcase.include?(key)}}.each do |att|
+				puts "\n\twriting attachment: #{att.filename}"
+				begin
+					File.open(@path + att.filename, "w+b", 0644) {|f| f.write att.body.decoded}
+					rescue Exception=> e
+						puts "Unable to save data for #{att.filename} because #{e.message}"
+				end
+			end
+		end
+		imap.logout 
+		imap.disconnect
+	end
+	
+	def process_file		
+		Dir[@path+"*.xlsx"].each do |file| 
 			puts "reading:"+ file
 			modify_file file
 		end
 	end
 	
 	def get_db_info sh_data
-		client = TinyTds::Client.new(:username => '', 
-									 :password => '', 
-									 :host => '')
+		client = TinyTds::Client.new(@sql_params)
 		#puts sh_data.first[1].to_s
-		case  
-		  when sh_data.first[1].to_s.start_with?("920")
-		    stype= "'Postpaid'"
-		  when sh_data.first[1].to_s.start_with?("9218")
-		    stype= "'Data'"
-		  else
-		    stype= "'Prepaid'"
+		case	
+			when sh_data.first[1].to_s.start_with?("920")
+				stype= "'Postpaid'"
+			when sh_data.first[1].to_s.start_with?("9218")
+				stype= "'Data'"
+			else
+				stype= "'Prepaid'"
 		end 
 
 		query = "WITH T1 AS( 
@@ -97,25 +100,24 @@ class Profile_Reader
 								ServiceType,DepositAmount,'' Service_List,'' provisiondate,IsCommitted, DepositBank,DepositDate,'' Packages, '' Delivery_Method 
 							FROM rightel.dbo.Prospect) 
 							
-						SELECT  NationalNo,msisdn,firstname,LastName,Nationality,Gender,
+						SELECT	NationalNo,msisdn,firstname,LastName,Nationality,Gender,
 								Title,IdentityNo,IssuePlace,MaritalStatus,FatherName,Birthdate,job,EducationLevel,EMail,PostalCode,CustomerType,
 								MobileNo,Tel,Province,City,ad1,Ave,ad2,Street,ad3,Description,Block,BuildingNo,Floor,Unit,MunicipalityRegion,ServiceType,
 								DepositAmount,Service_List,provisiondate,IsCommitted,DepositBank,DepositDate,Packages,Delivery_Method 
 						FROM T1 
 						WHERE nationalno IN ( "+sh_data.transpose.first.join(",")+" ) AND registrationlevel =1 AND ServiceType=" + stype +" ORDER BY NationalNo"
-		#puts query
-    client.execute(query).to_a
+		client.execute(query).to_a
 	end
 	
-	def modify_file filename
-		excel = Excelx.new filename
+	def modify_file file_path
+		excel = Excelx.new file_path
 		new_excel = Axlsx::Package.new
 		shs_data = {}
 		
 		return unless excel.cell(2,'A').downcase.include? 'national' 
 		
 		0.upto(excel.sheets.length-1) do |i|
-		  excel.default_sheet = excel.sheets[i]  
+			excel.default_sheet = excel.sheets[i]	 
 			key = excel.sheets[i].to_s
 			next if excel.first_row.nil? or !(/\d+/ =~ excel.cell(3,'A').to_s) 
 			nids = excel.column(1,excel.sheets[i])[2..-1]
@@ -125,34 +127,31 @@ class Profile_Reader
 		end
 		excel.default_sheet = excel.sheets[0]
 		shs_data.each do |sheet_name, sh_data| 
-		  next if sh_data.length < 1 
-		  result = get_db_info sh_data 
-	    new_excel.workbook do |wb|
-			  wb.add_worksheet(:name => sheet_name) do |ws|
-				  #debugger
-				  result.each_with_index do |row,i| 
-					  if i == 0 then
-					    ws.add_row excel.row(1)
-					    ws.add_row excel.row(2)
-					  end 
-					  #debugger if filename.include?("Cust Profile_CRM_online Payment_91051016")
-					  begin
-					    ws.add_row sh_data.find{|item| Integer(item[0]) == Integer(row.values[0])}.concat(row.values[2..-1]) 
-					  rescue
-					    @log.error "\n\n\n\terror for nationalno: "+row.values[0].to_s() + "in file:"+ filename
-					    ws.add_row []
-					  end
-				  end
-			  end
-		  end
-	  end
-		
-		new_filename = filename.gsub(/inbox\//,'').gsub(/.xlsx/,"_Filled_#{ DateTime.now.to_s.gsub(/:/,'')}.xlsx")
-		puts "writig #{new_filename}"
-		new_excel.serialize "#{new_filename}"
+			next unless sh_data.length > 0 
+			result = get_db_info sh_data 
+			new_excel.workbook do |wb|
+				wb.add_worksheet(:name=> sheet_name) do |ws|
+					result.each_with_index do |row,i| 
+						if i == 0 then
+							ws.add_row excel.row(1)
+							ws.add_row excel.row(2)
+						end
+						begin
+							ws.add_row sh_data.find{|item| Integer(item[0]) == Integer(row.values[0])}.concat(row.values[2..-1]) 
+						rescue
+							@log.error "\n\n\n\terror for nationalno: "+row.values[0].to_s() + "in file:"+ file_path
+							ws.add_row []
+						end
+					end
+				end
+			end
+		end
+		new_file_path = './'+ File.basename(file_path, ".xlsx")+ "_Filled_#{ DateTime.now.to_s.gsub(/:/,'')}.xlsx"
+		puts "writig #{new_file_path}"
+		new_excel.serialize "#{new_file_path}"
 	end
 end
 
-reader = Profile_Reader.new('j.zinedine','')
-#reader.email_read
+reader = Profile_Reader.new
+reader.email_read
 reader.process_file
